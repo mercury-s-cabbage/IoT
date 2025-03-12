a4c1e65acb7a2040e2ece7d0582aaa89c8849e4c
mer-lena <lenaphilip@mail.ru>
2025-03-12 15:17:58 +0800

    button matrix end !!

--- ButtonMatrix/sketch_mar12a/sketch_mar12a.ino
+++ ButtonMatrix/sketch_mar12a/sketch_mar12a.ino
@@ -13,7 +13,9 @@
 
 const int row_pins[NROWS] = {2, 3, 4};
 const int col_pins[NCOLS] = {5, 6, 7};
-bool btn_pressed[NCOLS] = {false, false, false};
+bool btn_pressed[NROWS][NCOLS] = {{false, false, false}, {false, false, false}, {false, false, false}};
+bool prev_btn_pressed[NROWS][NCOLS] = {{false, false, false}, {false, false, false}, {false, false, false}};
+unsigned long press_start_time[NROWS][NCOLS] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 volatile uint8_t current_row = 0;
 
 void setup_timer() {
@@ -47,20 +49,50 @@ ISR(TIMER1_COMPA_vect) {
   }
   digitalWrite(row_pins[current_row], LOW);
 
-  for(int icol = 0; icol < NCOLS; icol++) {
-    btn_pressed[icol] = !digitalRead(col_pins[icol]);
-  }
-  report_row_states(current_row);
+  bool state_changed = false;
+  unsigned long current_time = millis();
 
+  for(int icol = 0; icol < NCOLS; icol++) {
+    bool is_pressed = !digitalRead(col_pins[icol]);
+    if (is_pressed && !prev_btn_pressed[current_row][icol]) {
+      press_start_time[current_row][icol] = current_time;
+    }
+    if (!is_pressed && prev_btn_pressed[current_row][icol]) {
+      Serial.print("Button ");
+      Serial.print(1 + icol + NCOLS * current_row);
+      Serial.print(" duration: ");
+      Serial.print(current_time - press_start_time[current_row][icol]);
+      Serial.print(" ms, started at: ");
+      Serial.println(press_start_time[current_row][icol]);
+    }
+    btn_pressed[current_row][icol] = is_pressed;
+    if (btn_pressed[current_row][icol] != prev_btn_pressed[current_row][icol]) {
+      state_changed = true;
+    }
+  }
+  
+  if (state_changed) {
+    report_button_states();
+  }
+
+  memcpy(prev_btn_pressed, btn_pressed, sizeof(btn_pressed));
   current_row = (current_row + 1) % NROWS;
 }
 
-void report_row_states(int row_number) {
-  for(int icol = 0; icol < NCOLS; icol++) {
-    if(btn_pressed[icol] == true) {
-      Serial.print("Button ");
-      Serial.print(1 + icol + NCOLS * row_number);
-      Serial.println(" pressed");
+void report_button_states() {
+  Serial.print("Pressed buttons: ");
+  bool any_pressed = false;
+  for (int irow = 0; irow < NROWS; irow++) {
+    for (int icol = 0; icol < NCOLS; icol++) {
+      if (btn_pressed[irow][icol]) {
+        Serial.print(1 + icol + NCOLS * irow);
+        Serial.print(" ");
+        any_pressed = true;
+      }
     }
   }
+  if (!any_pressed) {
+    Serial.print("None");
+  }
+  Serial.println();
 }
